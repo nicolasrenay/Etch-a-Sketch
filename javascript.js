f9f59723f52bd05f4c76eefec106c1c04e59ab34
Nicolas <nicolas.renay@gmail.com>
2024-10-31 22:38:32 +0100

    adding randomize color

--- javascript.js
+++ javascript.js
@@ -1,29 +1,9 @@
 let gridContainer = document.querySelector('.grid-container');
 const container = document.getElementById('container');
-
 let size = 16
 
 
 buildGrid(size);
-
-function buildGrid (size) {
-    for (let i = 0; i < size*size; i++) {
-        let div = document.createElement('div');
-        let column = (1 / size) * 100 ;
-        div.className = "square-class";
-        div.setAttribute('id', 'square-id');
-        gridContainer.appendChild(div);
-        div.style.flexBasis = column + "%";
-    }
-    const pixel = document.getElementsByClassName('square-class');
-
-    for (let j = 0; j < pixel.length; j++) {
-    pixel[j].addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = "#69fc25";
-    });
-
-}
-}
 
 
 const btn = document.getElementsByClassName('question')[0];
@@ -38,15 +18,30 @@ btn.addEventListener('click', (event) => {
 
 });
 
-/*
-function removeGrid (size) {
-    for (let k = 0; k < size*size; k++) {
-       const selectDiv = document.getElementById('square-id');
-       selectDiv[k].remove();
+function buildGrid (size) {
+    for (let i = 0; i < size*size; i++) {
+        let div = document.createElement('div');
+        let column = (1 / size) * 100 ;
+        div.className = "square-class";
+        div.setAttribute('id', 'square-id');
+        gridContainer.appendChild(div);
+        div.style.flexBasis = column + "%";
     }
+    const pixel = document.getElementsByClassName('square-class');
+
+    for (let j = 0; j < pixel.length; j++) {
+    pixel[j].addEventListener("mouseover", (event) => {
+    let color = Math.floor(Math.random() * 10000);
+
+    event.target.style.backgroundColor = `#${color}`;
+    });
+
+}
 }
 
-*/
+
+
+
 
 function removeGrid (initsize) {
     for (let k = 0; k < initsize * initsize; k++) {
